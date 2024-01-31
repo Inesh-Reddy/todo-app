abf1b0d739ed11e3cfc0bf872740128a28addda5
Inesh-Reddy <ineshreddy007@gmail.com>
2024-01-31 11:32:13 +0530

    adding zod validation in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { createTodo, updateTodo } = require('./types');
 const app = express();
 
 
@@ -6,6 +7,15 @@ app.use(express.json());
 
 
 app.post("create-todo", function(req, res){
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "your inputs are wrong"
+        })
+        return;
+    }
+    //put it in mongoDB
 
 })
 
@@ -14,7 +24,15 @@ app.get("get-todo", function(req, res){
 })
 
 app.put("update-todo", function(req, res){
-    
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "not a valid one!"
+        })
+        return;
+    }
+    //update in MongoDB
 })
 
 app.listen(3000, function(){
